5c67738f33320b2e8c142c79c6bc7edada997854
unknown <plotkinm@LC112INSTPC.active.newpaltz.edu>
2014-08-28 10:37:33 -0400

    First Commit in Aptana

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<title>HTML</title>
+	</head>
+
+	<body> 
+		<div>
+			<header>
+				<h1>This is our first file</h1>
+			</header>
+			<nav>
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright  by plotkinm
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
