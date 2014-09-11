468ebd051a0900c28edd987a46559ca0b400f132
unknown <plotkinm@LC112INSTPC.active.newpaltz.edu>
2014-09-11 10:47:05 -0400

    Basic Bootstrap

--- playground/index.php
+++ playground/index.php
@@ -3,31 +3,39 @@
 	<head>
 
 		<title>HTML</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 	</head>
 
 	<body> 
 		<div>
 			<header>
-				<h1>This was edited in Aptana</h1>
-				<h2>Today is Sep 4th</h2>
+				<div class="container">
+					<h1>
+						<i class="glyphicon glyphicon-download-alt"></i>
+						This was edited in Aptana
+					</h1>
+					<h2>Today is Sep 4th</h2>
+				</div>
 			</header>
 			<nav>
-				<p>
+				<div class="container">
 					<a href="/">Home</a>
-				</p>
-				<p>
 					<a href="/contact">Contact</a>
-				</p>
+				</div>
 			</nav>
 
-			<div>
+			<div class="container">
 
 			</div>
 
 			<footer>
-				<p>
-					&copy; Copyright  by plotkinm
-				</p>
+				<div class="container">
+					<p>
+						&copy; Copyright  by plotkinm
+					</p>
+				</div>
 			</footer>
 		</div>
 	</body>
