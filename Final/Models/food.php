ef887864daabb4ddefaf6c9f706a99b31ad88e21
Moshe Plotkin <jewpaltz@gmail.com>
2014-10-20 07:11:58 -0400

    Centralize FetchAll

--- Final/Models/food.php
+++ Final/Models/food.php
@@ -7,17 +7,9 @@ class Food {
 	
 	public static function Get($id=null)
 	{
-		$ret = array();
 		$sql = "	SELECT * FROM 2014Fall_Food_Eaten
 		";
-		$conn = GetConnection();
-		$results = $conn->query($sql);
-		
-		while ($rs = $results->fetch_assoc()) {
-			$ret[] = $rs;
-		}
-		
-		return $ret;
+		return FetchAll($sql);
 	}
 }
 
--- Final/inc/_global.php
+++ Final/inc/_global.php
@@ -5,3 +5,20 @@ function GetConnection(){
 	include __DIR__ . '/_password.php';
 	return new mysqli('localhost','plotkinm',$sql_password,'plotkinm_db');
 }
+
+function FetchAll($sql){
+		$ret = array();
+		$conn = GetConnection();
+		$results = $conn->query($sql);
+		
+		$error = $conn->error;
+		if($error){
+			echo $error;
+		}else{
+			while ($rs = $results->fetch_assoc()) {
+				$ret[] = $rs;
+			}			
+		}
+		
+		return $ret;	
+}
